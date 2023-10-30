3ba02ef63c03c458331f93292e9f75af60ff8bf0
Bilashos <pasero.cm@gmail.com>
2023-10-30 23:55:22 +0100

    commento rotte

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 // Rotta per ottenere tutti gli utenti
 router.get('/', userController.getUser);
+// Rotta per creare un utente
 router.post('/create-user', userController.createUser);
 
 module.exports = router;
