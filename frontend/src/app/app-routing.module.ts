0461f18e644d60cf9336786ca4adc66f8575efc0
albianto97 <albyantox@gmail.com>
2023-11-27 16:54:50 +0100

    Rebase Profile eliminando branch non funzionante

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ import {UserListComponent} from "./components/user-list/user-list.component";
 import {ProductListComponent} from "./components/product-list/product-list.component";
 import {ProfiloComponent} from "./components/profile/profile.component";
 import {ProductCreationComponent} from "./components/product-creation/product-creation.component";
-import {SingleProductComponent} from "./components/single-product/single-product.component";
 
 const routes: Routes = [
   {path : 'login', component: LoginComponent},
@@ -16,7 +15,6 @@ const routes: Routes = [
   {path : 'productList', component: ProductListComponent},
   {path : 'single', component: ProfiloComponent},
   {path : 'create-product', component: ProductCreationComponent},
-  { path: 'single-product/:productId', component: SingleProductComponent },
 ];
 
 @NgModule({
