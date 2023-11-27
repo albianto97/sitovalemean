1e0038e9fa85ad7c5b6455e30210681490fb1b1c
albianto97 <albyantox@gmail.com>
2023-11-27 17:27:34 +0100

    Commit files

---
+++ frontend/src/app/components/single-product/single-product.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
+
+@Component({
+  selector: 'app-single-product',
+  templateUrl: './single-product.component.html',
+  styleUrls: ['./single-product.component.css']
+})
+export class SingleProductComponent implements OnInit {
+  product: Product | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) { }
+
+  async ngOnInit(): Promise<void> {
+    // Recupera l'ID del prodotto dalla route
+    const productId = this.route.snapshot.paramMap.get('productId');
+    console.log(productId);
+
+    // Chiamata al servizio per ottenere il singolo prodotto
+    if (productId) {
+      try {
+        this.product = await new Promise((resolve, reject) => {
+          this.productService.getSingleProduct(productId).subscribe(
+            (data) => resolve(data),
+            (error) => reject(error)
+          );
+        }) as Product;
+      } catch (error) {
+        console.error('Errore nel recupero del prodotto:', error);
+      }
+    }
+  }
+}
