220d2afcba29dfd154e67425a33bc480c1ba2eac
albianto97 <albyantox@gmail.com>
2023-12-13 16:48:18 +0100

    TODO file

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -28,6 +28,7 @@ export class AppRoutingModule { }
 
 
 // TODO: CRITTOGRAFARE PASSWORD;
-// TODO: SignIn controllo che non sia già presente nome ed email;  // testare --> errore in console se giusto?
-// TODO: Profile --> branch creato per singola persona --> con stato mettere la persona che ha fatto login;
+// TODO: branch SignIn --> controllo che non sia già presente nome ed email;  // testare --> errore in console (ispeziona) se giusto?
+// TODO: branch Profile --> branch creato per singola persona --> con stato mettere la persona che ha fatto login;
 // TODO: creazione prodotto fatta, bisogna gestire in maniera diversa il 400/404?
+// TODO: branch Ordine --> creazione ordine, rotte per accettazione e rifiuto fatte (da testare)
