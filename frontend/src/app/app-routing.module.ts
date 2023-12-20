10fced6d51a0523944d24ae59d738efabcef8e82
albianto97 <albyantox@gmail.com>
2023-12-20 22:52:47 +0100

    Modifica TODO

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -28,8 +28,5 @@ const routes: Routes = [
 export class AppRoutingModule { }
 
 
-// TODO: CRITTOGRAFARE PASSWORD;
-// TODO: branch SignIn --> controllo che non sia già presente nome ed email;  // testare --> errore in console (ispeziona) se giusto?
-// TODO: branch Profile --> branch creato per singola persona --> con stato mettere la persona che ha fatto login;
 // TODO: creazione prodotto fatta, bisogna gestire in maniera diversa il 400/404?
 // TODO: branch Ordine --> creazione ordine, rotte per accettazione e rifiuto fatte (da testare)
