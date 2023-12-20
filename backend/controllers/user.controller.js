b3d477958bc8000bc1637cc7f7ce312a1cdff7ba
Bilashos <pasero.cm@gmail.com>
2023-12-20 22:02:39 +0100

    aggiunto api login

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -14,13 +14,13 @@ const getSingleUser = async (req, res) => {
         const user = await User.findById(userId);
 
         if (!user) {
-            return res.status(404).json({message: 'Utente non trovato'});
+            return res.status(404).json({ message: 'Utente non trovato' });
         }
 
         res.json(user);
     } catch (error) {
         console.error('Errore durante il recupero dell\'utente:', error);
-        res.status(500).json({message: 'Errore del server'});
+        res.status(500).json({ message: 'Errore del server' });
     }
 };
 const createUser = async (req, res) => {
@@ -41,12 +41,25 @@ const createUser = async (req, res) => {
     }
 };
 
+const login = async (req, res) => {
+    try {
+        // Cerca l'utente nel database usando await
+        const user = await User.findOne({ email: req.body.email });
 
+        if (!user) return res.status(400).json({ isValid: false, message: "User not found" });
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
+            return res.status(400).json({ isValid: false, message: "Incorrect password" });
+        }
+        return res.status(200).json({ isValid: true, message: "Login successful" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ isValid: false, message: err.message });
+    }
+}
 
 module.exports = {
     getUser,
     createUser,
-    getSingleUser
-    createUser,
+    getSingleUser,
     login
 }
