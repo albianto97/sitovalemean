05a6c5823348764f3403c1bcec99de868d1867a0
albianto97 <albyantox@gmail.com>
2024-02-02 16:07:35 +0100

    Riparto dalla stampa dei prodotti, order non funziona

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -4,28 +4,35 @@ import { AuthService } from "../../services/auth.service";
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/models/product';
 
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
+
+
 export class ProfiloComponent implements OnInit {
   user: User | undefined;
   bestProducts: Product[] = [];
 
-  constructor(private auth: AuthService, private productService: ProductService) { }
-
+  constructor(private auth: AuthService,private productService: ProductService) { }
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  selectedType: string | null = null;
   ngOnInit(): void {
     this.user = this.auth.getUserFromToken();
 
-    this.productService.getBestProducts().subscribe((data: any) => {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+      console.log(this.products);
+
+      this.filteredProducts = this.products.splice(0,3);
+    });
+    /*this.productService.getBestProducts().subscribe((data: any) => {
         this.bestProducts = data;
         console.log(this.bestProducts);
-    });
+    });*/
 
   }
 }
-
-/*
- */
-
