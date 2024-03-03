fa784d977690a0d1387de2c41444116de5d23d01
albianto97 <albyantox@gmail.com>
2024-03-03 14:54:55 +0100

    Pulsanti Minus Plus da implementare, rotte modificate

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -32,7 +32,10 @@ export class LoginComponent {
       if (this.loginForm.valid) {
         var newUser = new User(this.loginForm.value.email, "username", this.loginForm.value.password);
         this.authService.login(newUser).subscribe((response: any) => {
-          this.router.navigate(['']);
+          if(!this.authService.isAdmin())
+            this.router.navigate(['']);
+          else
+            this.router.navigate(['/productList']);
         });
       }
     }
