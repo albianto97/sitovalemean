0f9490260f1d1464fa945b52aea0e7fd2558a1f6
albianto97 <albyantox@gmail.com>
2024-03-06 16:41:02 +0100

    Pulstante ritorno a principale

--- frontend/src/app/components/unauthorized/unauthorized.component.ts
+++ frontend/src/app/components/unauthorized/unauthorized.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-unauthorized',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./unauthorized.component.css']
 })
 export class UnauthorizedComponent {
+  constructor(private router: Router){}
 
+  navigateToHomePage() {
+    this.router.navigate(['/productList']);
+  }
 }
