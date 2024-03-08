c073e26e6c5d3adef64ffed43891aac907e548bc
albianto97 <albyantox@gmail.com>
2024-03-08 16:55:08 +0100

    Paginator --> filtrato

--- frontend/src/app/components/product/inventory/inventory.component.ts
+++ frontend/src/app/components/product/inventory/inventory.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 import { UserService } from 'src/app/services/user.service';
 import { AuthService } from 'src/app/services/auth.service';
+import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-inventory',
@@ -14,6 +15,7 @@ export class InventoryComponent implements OnInit {
   displayedProducts: Product[] = [];
   selectedType: string | null = null;
   maxVisualization: number = 6;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private productService: ProductService, public authService: AuthService) { }
 
@@ -27,6 +29,9 @@ export class InventoryComponent implements OnInit {
 
   filterProducts(type: string): void {
     this.selectedType = type;
+    if (this.paginator) {
+      this.paginator.firstPage();
+    }
     if (type === 'TORTA' || type === 'GELATO') {
       this.displayedProducts = this.products.filter((product) => product.type === type).slice(0, this.maxVisualization);
     } else {
@@ -37,7 +42,11 @@ export class InventoryComponent implements OnInit {
   onPageChange(event: any): void {
     const startIndex = event.pageIndex * event.pageSize;
     const endIndex = startIndex + event.pageSize;
-    this.displayedProducts = this.products.slice(startIndex, endIndex);
+    if (this.selectedType === '' || this.selectedType === null) {
+      this.displayedProducts = this.products.slice(startIndex, endIndex);
+    } else {
+      this.displayedProducts = this.products.filter((product) => product.type === this.selectedType).slice(startIndex, endIndex);
+    }
     this.maxVisualization = endIndex;
   }
 }
