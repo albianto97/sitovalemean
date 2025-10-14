9c45c20e7114e0d5b3f7cde31b2c193847012589
albianto97 <36625340+albianto97@users.noreply.github.com>
2025-10-14 12:59:04 +0200

    Update reservation-cart.component.ts

--- frontend/src/app/components/reservation-cart/reservation-cart.component.ts
+++ frontend/src/app/components/reservation-cart/reservation-cart.component.ts
@@ -6,6 +6,45 @@ import { Component } from '@angular/core';
   templateUrl: './reservation-cart.component.html',
   styleUrl: './reservation-cart.component.css'
 })
-export class ReservationCartComponent {
-  reservations = ['Regalo 1', 'Regalo 2'];
+export class ReservationCartComponent implements OnInit {
+  products: any[] = [];
+  loading = true;
+
+  constructor(private reservationService: ReservationService) {}
+
+  ngOnInit(): void {
+    this.loadReservations();
+  }
+
+  loadReservations(): void {
+    this.loading = true;
+    this.reservationService.getMyReservations().subscribe({
+      next: (res) => {
+        this.products = res.products || [];
+        this.loading = false;
+      },
+      error: () => {
+        alert('Errore nel caricamento delle prenotazioni.');
+        this.loading = false;
+      }
+    });
+  }
+
+  removeItem(productId: string) {
+    this.reservationService.removeProduct(productId).subscribe({
+      next: () => {
+        this.products = this.products.filter(p => p.productId._id !== productId);
+      },
+      error: () => alert('Errore nel rilascio del prodotto.')
+    });
+  }
+
+  clearAll() {
+    if (confirm('Vuoi davvero svuotare tutte le prenotazioni?')) {
+      this.reservationService.clearReservations().subscribe({
+        next: () => (this.products = []),
+        error: () => alert('Errore nello svuotamento prenotazioni.')
+      });
+    }
+  }
 }
