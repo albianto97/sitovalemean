a3806d35b3f327a88886638bfd4534dfcc11c024
albianto97 <36625340+albianto97@users.noreply.github.com>
2025-10-16 17:24:54 +0200

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -8,6 +8,8 @@ router.post('/register', registerUser);
 router.post('/login', login);
 router.get('/me', authMiddleware, me);
 router.put('/update', authMiddleware, updateUser);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
 
 
 export default router;
