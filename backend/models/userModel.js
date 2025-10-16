194eed5483d663d16161345eca29b6066bedfcaf
albianto97 <36625340+albianto97@users.noreply.github.com>
2025-10-16 17:26:12 +0200

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,14 +2,17 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
-    {
-        username: { type: String, required: true, unique: true,trim: true, lowercase: true  },
-        email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-        password: { type: String, required: true },
-        role: { type: String, enum: ['user', 'admin'], default: 'user' }
-    },
-    { timestamps: true }
+  {
+    username: { type: String, required: true, unique: true, trim: true, lowercase: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+    resetToken: String,
+    resetTokenExpire: Date,
+  },
+  { timestamps: true }
 );
+
 
 // 👇 Middleware per assicurarsi che l'email sia in lowercase anche se inviata in maiuscolo
 userSchema.pre('save', function (next) {
