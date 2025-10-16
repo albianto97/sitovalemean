c163bce44723ebf3380b6aef8a14bc194264312e
albianto97 <36625340+albianto97@users.noreply.github.com>
2025-10-16 15:34:34 +0200

    Create setupTestDB.js

---
+++ backend/tests/setupTestDB.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+let mongoServer;
+
+export const connectTestDB = async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri);
+};
+
+export const closeTestDB = async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+};
