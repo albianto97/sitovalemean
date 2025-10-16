42d2937382e64b8a30f5adf4ee38c8416a8137bc
albianto97 <36625340+albianto97@users.noreply.github.com>
2025-10-16 18:15:53 +0200

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import {login, me, registerUser, updateUser} from '../controllers/authController.js';
+import {login, me, registerUser, updateUser, changePassword} from '../controllers/authController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const router = Router();
@@ -10,6 +10,8 @@ router.get('/me', authMiddleware, me);
 router.put('/update', authMiddleware, updateUser);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:token', resetPassword);
+router.post('/change-password', authMiddleware, changePassword);
+
 
 
 export default router;
